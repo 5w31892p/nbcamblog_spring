35b8fcbb049ef245d92712d1e023159d0ef3966a
Seowoo <5w31892p@gmail.com>
2022-12-16 11:56:40 +0900

    @RestController으로 변경 & signup/login responsedto 적용

--- src/main/java/com/sparta/nbcamblog/controller/UserController.java
+++ src/main/java/com/sparta/nbcamblog/controller/UserController.java
@@ -1,34 +1,32 @@
 package com.sparta.nbcamblog.controller;
 
 import com.sparta.nbcamblog.dto.LoginRequestDto;
+import com.sparta.nbcamblog.dto.LoginResponseDto;
 import com.sparta.nbcamblog.dto.SignupRequestDto;
+import com.sparta.nbcamblog.dto.SignupResponseDto;
 import com.sparta.nbcamblog.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@RequestMapping("/api/user")
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
+    public SignupResponseDto signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         userService.signup(signupRequestDto);
-        return "회원가입에 성공하였습니다.";
+        return new SignupResponseDto();
     }
 
     @ResponseBody
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+    public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
         userService.login(loginRequestDto, response);
-        return "로그인이 완료되었습니다.";
+        return new LoginResponseDto();
     }
 }
