f70dc88ca8ac837865c29d31b225b925f6746302
Seowoo <5w31892p@gmail.com>
2022-12-19 11:03:37 +0900

    username 정규식표현 수정

--- src/main/java/com/sparta/nbcamblog/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/nbcamblog/dto/SignupRequestDto.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 public class SignupRequestDto {
-    @Pattern(regexp="^[a-z0-9]{4,10}$", message = "아이디는 영어(소문자)와 숫자를 포함해서 4~10자리 이내로 입력해주세요.")
+    @Pattern(regexp="^.*(?=^.{4,10}$)(?=.*\\d)(?=.*[a-z]).*$", message = "아이디는 영어(소문자)와 숫자를 포함해서 4~10자리 이내로 입력해주세요.")
     private String username;
     @Pattern(regexp="^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$", message = "비밀번호는 영어(대소문자)와 숫자, 특수문자를 포함해서 8~15자리 이내로 입력해주세요.")
     private String password;
