bf0f650b01fc1b7049ad9f5f081e15f1020937ed
Seowoo <5w31892p@gmail.com>
2022-12-19 10:57:30 +0900

    정규식표현 SignupRequestDto로 이동 및 비밀번호 정규식표현 특수문자 추가

--- src/main/java/com/sparta/nbcamblog/entity/User.java
+++ src/main/java/com/sparta/nbcamblog/entity/User.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +16,9 @@ public class User {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    @Pattern(regexp="^[a-z0-9]{4,10}$", message = "아이디는 영어(소문자)와 숫자를 포함해서 4~10자리 이내로 입력해주세요.")
     private String username;
 
     @Column(nullable = false)
-    @Pattern(regexp="^[a-zA-Z0-9]{8,15}$", message = "비밀번호는 영어(대소문자)와 숫자를 포함해서 4~10자리 이내로 입력해주세요.")
     private String password;
 
 
