2f775b01d53127149879df0efe470c6643b7f7ba
Seowoo <5w31892p@gmail.com>
2022-12-30 22:47:55 +0900

    test : signup test attempted

--- src/test/java/com/sparta/nbcamblog/NbcamBlogApplicationTests.java
+++ src/test/java/com/sparta/nbcamblog/NbcamBlogApplicationTests.java
@@ -1,13 +1,27 @@
 package com.sparta.nbcamblog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sparta.nbcamblog.dto.SignupRequestDto;
+import com.sparta.nbcamblog.security.UserSecurityService;
+import com.sparta.nbcamblog.service.UserService;
 
 @SpringBootTest
 class NbcamBlogApplicationTests {
 
+    @Autowired
+    private UserSecurityService securityService;
+    @Autowired
+    private UserService service;
+    @Autowired
     @Test
     void contextLoads() {
+        String username = "asf123";
+        String password = "asdf123!!";
+        boolean admin = false;
+        this.service.signup(new SignupRequestDto());
     }
 
 }
